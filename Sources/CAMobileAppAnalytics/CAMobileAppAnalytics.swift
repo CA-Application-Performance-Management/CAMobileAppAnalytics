9260b720d62766b44e05343fb8c8b2e0dce4035b
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2023-08-21 15:28:21 +0530

    no message

--- Sources/CAMobileAppAnalytics/CAMobileAppAnalytics.swift
+++ Sources/CAMobileAppAnalytics/CAMobileAppAnalytics.swift
@@ -1,6 +1,7 @@
+import Foundation
 public struct CAMobileAppAnalytics {
     public private(set) var text = "Hello, World!"
-
+    public static var CAMobileAppAnalytics_Bundle: Bundle { Bundle.module }
     public init() {
     }
 }
