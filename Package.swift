a1468bdd5357d134f1d0c8219c4bfed399179359
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2025-11-22 19:02:35 +0530

    Sunlife requirement - SDK swift package integration in another swift package Manager
    Solution: as bridging-header not able to be used in a Package, need to add wrapper

--- Package.swift
+++ Package.swift
@@ -7,35 +7,35 @@ let package = Package(
     name: "CAMobileAppAnalytics",
     platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CAMobileAppAnalytics",
             targets: ["CAMobileAppAnalytics"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-    .target(
-        name: "CAMobileAppAnalytics",
-        dependencies: [
         .target(
-              name: "xcframework",
-              condition: .when(platforms: [.iOS])
-            ),
-        ],
-        path: "./Sources/CAMobileAppAnalytics/",
-        exclude: ["Classes/libCAMobileAppAnalytics.a", "Classes/libCAMobileAppAnalytics-simulator.a"],
-        resources: [
-               .process("Resources")
-        ],
-        cSettings: [
-            .headerSearchPath("Classes")
-        ],
-        linkerSettings: [.unsafeFlags(["-ObjC"])]
-        ),  
-        .binaryTarget(name: "xcframework",
-                      path: "./Sources/CAMobileAppAnalytics/CAMobileAppAnalytics.xcframework"),
+            name: "CAMobileAppAnalytics",
+            dependencies: [
+                "CAMobileAppAnalyticsWrapper"
+            ],
+            path: "./Sources/CAMobileAppAnalytics/",
+            exclude: ["Classes/libCAMobileAppAnalytics.a", "Classes/libCAMobileAppAnalytics-simulator.a"],
+            resources: [
+                   .process("Resources")
+            ],
+        ),
+        // C/ObjC Wrapper that exposes public headers
+        .target(
+            name: "CAMobileAppAnalyticsWrapper",
+            dependencies: [ "CAMobileAppAnalyticsBinary"],
+            path: "Sources/Wrapper",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("../CAMobileAppAnalytics/Classes/"),
+            ],
+            linkerSettings: [.unsafeFlags(["-ObjC"])]
+        ),
+        .binaryTarget(
+            name: "CAMobileAppAnalyticsBinary",
+            path: "./Sources/CAMobileAppAnalytics/CAMobileAppAnalytics.xcframework")
     ]
 )
--- Sources/CAMobileAppAnalytics/CAMobileAppAnalytics.swift
+++ Sources/CAMobileAppAnalytics/CAMobileAppAnalytics.swift
@@ -1,4 +1,5 @@
 import Foundation
+@_exported import CAMobileAppAnalyticsWrapper
 public struct CAMobileAppAnalytics {
     public init() {
     }
